93507c22a9989c27668215aacd878f12c5ca0747
Krushna <ankadekrushna01@gmail.com>
2025-06-21 21:47:19 +0530

    made a page and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full h-20 bg-black '>
+       <div className='text-white h-full flex gap-10 justify-center items-center    '>
+        <h3 className='text-[#d7dbdb] text-sm tracking-tighter cursor-pointer'>SpaceX © 2025</h3>
+        <h3 className='text-sm font-semibold tracking-tight text-[#c7c7c7] cursor-pointer'>PRIVACY POLICY</h3>
+        <h3 className='text-sm font-semibold text-[#bdbbbb] cursor-pointer'>SUPPLIERS</h3>
+       </div>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Footer from './Footer'
 
 const Home = () => {
   return (
@@ -10,9 +11,24 @@ const Home = () => {
           <h1>AX-4 Mission</h1>
           <button className='px-5 py-6 border-[1px] border-white'>Learn More</button>
         </div> */}
-        <div className='bg-[url(/Mars.jpg)] bg-cover bg-center h-[80%] '>
-
+        <div className='bg-[url(/Mars.jpg)] bg-cover bg-center h-[95%] bg-no-repeat relative'>
+          <div className=' w-[500px]  absolute top-[55%] left-28'>
+            <h1 className='text-4xl font-bold text-white tracking-wider leading-10'>THE ROAD TO MAKING <br /> LIFE MULTIPLANETARY</h1>
+           <button className="px-10 py-3 border-[2px] border-white text-1xl font-semibold text-white outline-none mt-8 transition delay-120 hover:scale-125 hover:bg-white hover:text-black ">
+          Learn More
+        </button>
+          </div>
         </div>
+         <div className='bg-[url(/rocket.jpg)] bg-cover bg-center h-full relative'>
+ <div className=' w-[500px]  absolute top-[55%] left-28'>
+            <p className='uppercase text-[#fcfdfd]'>Recent Launch</p>
+            <h1 className='text-4xl font-bold text-[#ffffff] tracking-wider leading-10'>Starship's Ninth <br />Flight Test</h1>
+           <button className="px-10 py-3 border-[2px] border-white text-md font-semibold text-white outline-none mt-8 transition delay-120 hover:scale-125 hover:bg-white hover:text-black uppercase">
+          Rewatch
+        </button>
+          </div>
+        </div>
+        <Footer/>
     </div>
   )
 }
